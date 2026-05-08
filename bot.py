bab458256dbd2b84fb6337b4fdb50b4d027e9d98
aliounebathily26-cpu <aliounebathily26@gmail.com>
2026-05-08 20:29:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import time
 import requests
 from dotenv import load_dotenv
 
@@ -7,11 +8,13 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+
 def send_telegram(message):
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
         raise ValueError("Variables Telegram manquantes")
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message,
@@ -21,8 +24,14 @@ def send_telegram(message):
     response = requests.post(url, json=payload, timeout=10)
     print(response.status_code, response.text)
 
+
 def main():
     send_telegram("🤖 Bot Polymarket démarré. Base propre.")
+    print("Bot en ligne. Attente active.")
+
+    while True:
+        time.sleep(60)
+
 
 if __name__ == "__main__":
     main()
